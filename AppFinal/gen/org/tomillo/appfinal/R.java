b93195c8a99e91ec5a3c994d0455fb52f7073a28
Android <Android@51Profesor.aulatomillo.org>
2015-07-16 18:48:42 +0200

    Imagenes

--- AppFinal/gen/org/tomillo/appfinal/R.java
+++ AppFinal/gen/org/tomillo/appfinal/R.java
@@ -11,13 +11,23 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int btconfi=0x7f020001;
+        public static final int btintro=0x7f020002;
+        public static final int btjugar=0x7f020003;
+        public static final int btrecord=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int nombre=0x7f020006;
     }
     public static final class id {
+        public static final int btnConfiguracion=0x7f060003;
+        public static final int btnJugar=0x7f060002;
+        public static final int btnRecords=0x7f060001;
         public static final int textView1=0x7f060000;
     }
     public static final class layout {
         public static final int activity_splash=0x7f030000;
+        public static final int jugar=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
